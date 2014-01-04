0fd6230af9bbc4caa6680573769d041b3b0cc191
rougesheep <rougesheep@gmail.com>
2014-01-04 22:37:44 +0000

    Initial Twitch Support
    Added initial handling of twitch.tv urls. Only works for live streams
    right now. VODs soon.
    
    Options page exists, but non-functional.

--- background.js
+++ background.js
@@ -5,6 +5,9 @@ function checkForValidUrl(tabId, changeInfo, tab) {
     // ... show the page action.
     chrome.pageAction.show(tabId);
   }
+  else if (tab.url.indexOf('twitch.tv/') > -1) {
+    chrome.pageAction.show(tabId);
+  };
 };
 
 // Listen for any changes to the URL of any tab.
@@ -12,13 +15,25 @@ chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 chrome.pageAction.onClicked.addListener(function(tab){
   //var thisTab = tab.url;
-  //if (thisTab != "www") //ignore main site
-  var string = tab.url.split("v=");
-  var embedded = "https://www.youtube.com/embed/" + string[1];
-  //Reload current tab with embedded URL
-  //chrome.tabs.update(tab.id, {url: embedded});
-  //Close current tab
-  chrome.tabs.remove(tab.id);
-  //Open embedded URL in a panel
-  chrome.windows.create({url: embedded, type: "panel", height: 376, width: 600});
+
+  if (tab.url.indexOf("youtube") != -1) {
+    var string = tab.url.split("v=");
+    var embedded = "https://www.youtube.com/embed/" + string[1];
+    //Reload current tab with embedded URL
+    //chrome.tabs.update(tab.id, {url: embedded});
+    //Close current tab
+    chrome.tabs.remove(tab.id);
+    //Open embedded URL in a panel
+    chrome.windows.create({url: embedded, type: "panel", height: 376, width: 600});
+  }
+  else if (tab.url.indexOf("twitch") != -1) {
+    var string = tab.url.split("/");
+    var embedded = "http://www.twitch.tv/" + string[3] + "/popout"
+    chrome.windows.create({url: embedded, type: "panel", height: 376, width: 600});
+  }
+  else {
+    alert("other");
+  }
+
+  
 });
